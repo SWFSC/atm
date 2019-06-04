004637ce74a9a95872a59cd15a6c7732f3d329fd
Kevin Stierhoff <kevins@ucsc.edu>
2019-06-04 13:42:34 -0700

    Split ctd extraction into two separate functions, extract_ctd_header() and extract_ctd_cast(). Updated documentation.

--- R/ctd.R
+++ R/ctd.R
@@ -1,16 +1,10 @@
-# header.filename <- "C:/CODE/atm/data/CTD/test.hdr"
-# cast.filename   <- "C:/CODE/atm/data/CTD/test_processed.asc"
-#
-header.filename <- "C:/CODE/atm/data/UCTD/test.asc"
-cast.filename   <- "C:/CODE/atm/data/UCTD/test_processed.asc"
-
-#' Extract header and cast information from CTD and UCTD casts.
+#' Extract header information from CTD and UCTD casts.
 #'
 #' @param header.filename Name of header file.
-#' @param cast.filename Name of cast file.
 #' @param type Cast type (CTD or UCTD).
+#' @return A data frame containing header information.
 #' @export
-extract_ctd <- function(header.filename, cast.filename, type) {
+extract_ctd_header <- function(header.filename, type) {
   if (type == "UCTD") {
     # Process UCTD header file -----------------------------------------------------
     # Read header text
@@ -35,22 +29,6 @@ extract_ctd <- function(header.filename, cast.filename, type) {
     sn <- as.numeric(stringr::str_extract(
       unlist(stringr::str_extract_all(header.txt,
                                       pattern = '\\*SerialNumber=\\d{8}'))[1],"\\d{8}"))
-
-    # Process UCTD cast -------------------------------------------------------
-    # Read cast data and rename columns
-    cast <- read.table(cast.filename, header = TRUE) %>%
-      dplyr::rename(scan = Scan, C = C0S.m, 'T' = Tnc90C, P = PrM,
-             Z = DepSM, S = Sal00, Sv = SvCM, avgsVCM = AvgsvCM,
-             Dens = Density00, Flag = Flag) %>%
-      dplyr::mutate(
-        scan = scan - 1,
-        s    = scan/16,       # Calculate time (s) from scan (scan rate is 16 Hz)
-        dt   = c(0, diff(s)), # Calculate time interval
-        dZ   = c(1, diff(Z)), # Calculate change in depth (dZ, m)
-        dZt  =  as.numeric(forecast::ma(dZ/dt, order = 5)),
-        Z    = -Z,
-        cast = cast.filename)
-
   } else if (type == "CTD") {
     # Process CTD header file -----------------------------------------------------
     # Read header text
@@ -71,20 +49,43 @@ extract_ctd <- function(header.filename, cast.filename, type) {
     }
     # Set serial number to NA
     sn <- NA
+  }
 
+  # Combine data frames for export
+  data.frame(cast = header.filename, cast.date = cast.date, sn = sn)
+}
+
+#' Extract cast data from CTD and UCTD casts.
+#'
+#' @param cast.filename Name of cast file.
+#' @param type Cast type (CTD or UCTD).
+#' @return A data frame containing cast data.
+#' @export
+extract_ctd_cast <- function(cast.filename, type) {
+  if (type == "UCTD") {
+    # Process UCTD cast -------------------------------------------------------
+    # Read cast data and rename columns
+    read.table(cast.filename, header = TRUE) %>%
+      dplyr::rename(scan = Scan, C = C0S.m, 'T' = Tnc90C, P = PrM,
+                    Z = DepSM, S = Sal00, Sv = SvCM, avgsVCM = AvgsvCM,
+                    Dens = Density00, Flag = Flag) %>%
+      dplyr::mutate(
+        scan = scan - 1,
+        s    = scan/16,       # Calculate time (s) from scan (scan rate is 16 Hz)
+        dt   = c(0, diff(s)), # Calculate time interval
+        dZ   = c(1, diff(Z)), # Calculate change in depth (dZ, m)
+        dZt  =  as.numeric(forecast::ma(dZ/dt, order = 5)),
+        Z    = -Z,
+        cast = cast.filename)
+
+  } else if (type == "CTD") {
     # Process CTD cast --------------------------------------------------------
     # Read cast data and rename columns
-    cast <- read.table(cast.filename, header = TRUE) %>%
+    read.table(cast.filename, header = TRUE) %>%
       dplyr::rename(P = PrDM, 'T' = T090C, C = C0S.m, Z = DepSM, S = Sal00,
                     Sv = SvCM, avgsvCM = AvgsvCM, Dens = Density00, Flag = Flag) %>%
       dplyr::mutate(
         Z    = -Z, # Make depth negative
         cast = cast.filename)
   }
-
-  # Combine data frames for export
-  header <- data.frame(cast = header.filename, cast.date = cast.date, sn = sn)
-
-  # Export header and cast data as a list
-  list(header = header, cast = cast)
 }
