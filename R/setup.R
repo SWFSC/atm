9842b27557f62d80f5f616abca70888110e262a1
Kevin Stierhoff <kevins@ucsc.edu>
2020-03-26 10:36:52 -0700

    Added directory creation in clone_estimATM(). Created test script for cloning the repository.

---
+++ R/setup.R
@@ -0,0 +1,30 @@
+#' Clone estimATM repository
+#'
+#' @param repo_url URL for estimATM repository on GitHub.
+#' @param path Path to desired project location.
+#' @param overwrite Overwrite existing directory.
+#' @return
+#' @export
+#'
+#' @examples
+clone_estimATM <- function(repo_url = "https://github.com/kstierhoff/estimATM", path, overwrite = FALSE) {
+  if (exists(path)) {
+    if (overwrite) {
+      # Clone repository
+      git2r::clone(repo_url, path)
+
+    } else {
+      # Halt
+      print("Error, not overwriting existing directory")
+    }
+  } else {
+    # Create new directory
+    dir.create(path)
+
+    # Clone repository
+    git2r::clone(repo_url, path)
+  }
+
+  # Create required directories
+  fs::dir_create(paste(path, c("Data", "Figs","Output")))
+}
---
+++ tests/test_setup.R
@@ -0,0 +1,4 @@
+# Test repository cloning
+test.path <- file.path("C:/Users/kevin.stierhoff/Desktop/clone_test","test1")
+
+clone_estimATM(path = test.path)
