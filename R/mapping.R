525e5e46ef38714590c192658c4f6a82f7b80edc
Kevin Stierhoff <kevins@ucsc.edu>
2020-05-05 00:33:39 -0700

    Minor fix to get_basemap(), to fix error when adding ggspatial::annotation_scale() after defining xlim/ylim.

--- R/mapping.R
+++ R/mapping.R
@@ -39,8 +39,8 @@ get_basemap <- function(nav, states, countries, labels, bathy, boundaries = NULL
     # Format axes and titles
     xlab("Longitude") + ylab("Latitude") +
     ggplot2::coord_sf(crs = crs,
-             xlim = c(map.bounds["xmin"], map.bounds["xmax"]),
-             ylim = c(map.bounds["ymin"], map.bounds["ymax"])) +
+             xlim = unname(c(map.bounds["xmin"], map.bounds["xmax"])),
+             ylim = unname(c(map.bounds["ymin"], map.bounds["ymax"]))) +
     theme_bw() +
     theme(axis.text.y          = element_text(angle = 90, hjust = 0.5),
           legend.position      = c(0,0),
