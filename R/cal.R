ce1bda064163073d03f453f0b2da776ce0ef9b4a
Kevin Stierhoff <kevins@ucsc.edu>
2022-12-16 15:13:04 -0800

    Added function extract_cal_ecs, for extracting calibration information from Echoview calibration supplement files.

--- R/cal.R
+++ R/cal.R
@@ -1,4 +1,4 @@
-#' Extract calibration results.
+#' Extract calibration results from .txt or .xml files.
 #' @description Extract calibration results from ER60 (.txt) or EK80 (.xml) software.
 #' @param filename An string containing the name of the calibration file.
 #' @param vessel.name A string containing the vessel name.
@@ -566,4 +566,56 @@ extract_cal_fm <- function(filename, vessel.name = NA_character_, survey.name = 
 
   # Return calibration information as a list of tibbles
   list(cal.res = cal.res)
+}
+
+#' Extract calibration results from Echoview calibration supplement (.ecs) files.
+#' @description Extract calibration results from Echoview calibration supplement (.ecs) files.
+#' @param filename An string containing the name of the calibration file.
+#' @return A data frame containing calibration results.
+#' @export
+extract_cal_ecs <- function(filename) {
+
+  # Read calibration file
+  cal     <- readLines(filename)
+
+  # extract calibration results
+  Freq          <- as.numeric(stringr::str_extract(unlist(
+    stringr::str_extract_all(cal, pattern = '\\s+Frequency\\s*=\\s*\\S+')),"[-0-9]{1,}\\.[0-9]{1,}"))
+  BW.athwt      <- as.numeric(stringr::str_extract(unlist(
+    stringr::str_extract_all(cal, pattern = '^[^#]*\\s*MajorAxis3dbBeamAngle\\s*=\\s*\\S+')),"[-0-9]{1,}\\.[0-9]{1,}"))
+  BW.along      <- as.numeric(stringr::str_extract(unlist(
+    stringr::str_extract_all(cal, pattern = '^[^#]*\\s*MinorAxis3dbBeamAngle\\s*=\\s*\\S+')),"[-0-9]{1,}\\.[0-9]{1,}"))
+  Offset.athwt  <- as.numeric(stringr::str_extract(unlist(
+    stringr::str_extract_all(cal, pattern = '^[^#]*\\s*MajorAxisAngleOffset\\s*=\\s*\\S+')),"[-0-9]{1,}\\.[0-9]{1,}"))
+  Offset.along  <- as.numeric(stringr::str_extract(unlist(
+    stringr::str_extract_all(cal, pattern = '^[^#]*\\s*MinorAxisAngleOffset\\s*=\\s*\\S+')),"[-0-9]{1,}\\.[0-9]{1,}"))
+  Sa.corr       <- as.numeric(stringr::str_extract(unlist(
+    stringr::str_extract_all(cal, pattern = '^[^#]*\\s*SaCorrectionFactor\\s*=\\s*\\S+')),"[-0-9]{1,}\\.[0-9]{1,}"))
+  Gain          <- as.numeric(stringr::str_extract(unlist(
+    stringr::str_extract_all(cal, pattern = '^[^#]*\\s*TransducerGain\\s*=\\s*\\S+')),"[-0-9]{1,}\\.[0-9]{1,}"))
+  EBA           <- as.numeric(stringr::str_extract(unlist(
+    stringr::str_extract_all(cal, pattern = '^[^#]*\\s*TwoWayBeamAngle\\s*=\\s*\\S+')),"[-0-9]{1,}\\.[0-9]{1,}"))
+
+  # create a tibble of calibration results
+  cal.res <- tibble::tibble(Freq,
+                            BW.athwt,
+                            BW.along,
+                            Offset.athwt,
+                            Offset.along,
+                            Sa.corr,
+                            Gain,
+                            EBA)
+
+  # Define column names
+  names(cal.res) <- c("Frequency",
+                      "Beamwidth_athwartship",
+                      "Beamwidth_alongship",
+                      "OffsetAngle_athwartship",
+                      "OffsetAngle_alongship",
+                      "Sa_correction",
+                      "Gain",
+                      "TwoWayBeamAngle")
+
+  # Return the cal.res variable
+  return(cal.res)
 }
